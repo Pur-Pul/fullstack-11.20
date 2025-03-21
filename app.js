b3598324db8002834a4859c7d0938d2cdc3b040d
PurPul <axbergsamuel@gmail.com>
2025-03-21 17:38:42 +0200

    Fixed health endpoint

--- app.js
+++ app.js
@@ -9,10 +9,12 @@ const testingRouter = require('./controllers/testing')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
+const healthRouter = require('./controllers/health')
 
 logger.info(`connecting to ${config.MONGODB_URI}`)
 
-mongoose.connect(config.MONGODB_URI)
+mongoose
+	.connect(config.MONGODB_URI)
 	.then(() => {
 		logger.info('connected to MongoDB')
 	})
@@ -29,12 +31,12 @@ app.use(middleware.tokenExtractor)
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
+app.use('/health', healthRouter)
 
 if (process.env.NODE_ENV === 'test') {
 	const testingRouter = require('./controllers/testing')
 	app.use('/api/testing', testingRouter)
 }
-
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
--- index.js
+++ index.js
@@ -5,7 +5,3 @@ const logger = require('./utils/logger')
 app.listen(config.PORT, () => {
 	logger.info(`Server running on port ${config.PORT}`)
 })
-
-app.get('/health', (req, res) => {
-	res.send('ok')
-})
