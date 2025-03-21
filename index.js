4834b14aa6c5ded6680d19fc888670fc3d822673
PurPul <axbergsamuel@gmail.com>
2025-03-21 17:23:54 +0200

    health check

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ const logger = require('./utils/logger')
 app.listen(config.PORT, () => {
 	logger.info(`Server running on port ${config.PORT}`)
 })
+
+app.get('/health', (req, res) => {
+	res.send('ok')
+})
